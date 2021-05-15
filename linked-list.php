f9107bc2e824aa4a0b261b8486b594216a8724da
ngocphuocha <phuoc04012000@gmail.com>
2021-05-15 14:50:58 +0700

    init

--- linked-list.php
+++ linked-list.php
@@ -2,84 +2,99 @@
 
 class Node
 {
-    public $data;
-    public $next;
+  public $data;
+  public $next;
 }
 
 class LinkedList
 {
-    public $head;
-    public function __construct()
-    {
-        $this->head = null; // create empty linked list
+  public $head;
+  public function __construct()
+  {
+    $this->head = null; // create empty linked list
+  }
+
+
+  /**
+   * Add new element at the end of the list
+   *
+   * @param  mixed $newElement
+   * @return void
+   */
+  public function append($newElement)
+  {
+    $newNode = new Node();
+    $newNode->data = $newElement;
+    $newNode->next = null;
+
+    $temp = new Node();
+    $temp = $this->head;
+
+    if ($temp == null) {
+      $this->head = $newNode;
+    } else {
+      while ($temp->next != null) {
+        $temp = $temp->next;
+      }
+      $temp->next = $newNode;
     }
+  }
 
-    //Add new element at the end of the list
-    public function append($newElement)
-    {
-        $newNode = new Node();
-        $newNode->data = $newElement;
-        $newNode->next = null;
+  /**
+   * Add new element before the head of linked list
+   *
+   * @param  mixed $data
+   * @return void
+   */
+  public function prepend($data)
+  {
+    $newHead = new Node();
+    $newHead->data = $data;
+    $newHead->next = $this->head;
+    $this->head = $newHead; // change pointer of the head = $new head
+  }
 
-        $temp = new Node();
-        $temp = $this->head;
-
-        if ($temp == null) {
-            $this->head = $newNode;
-        } else {
-            while ($temp->next != null) {
-                $temp = $temp->next;
-            }
-            $temp->next = $newNode;
-        }
+  public  function deleteWithValue($data)
+  {
+    $temp = new Node();
+    $temp = $this->head;
+    if ($temp == null) {
+      return;
     }
-
-    public function prepend($data)
-    {
-        $newHead = new Node();
-        $newHead->data = $data;
-        $newHead->next = $this->head;
-        $this->head = $newHead; // change pointer of the head = $new head
+    // If want delete head
+    if ($temp->data == $data) {
+      $this->head = $temp->next; // move pointer from head to the next node
+      return;
     }
-
-    public  function deleteWithValue($data)
-    {
-        $temp = new Node();
-        $temp = $this->head;
-        if ($temp == null) {
-            return;
-        }
-        //if want delete head
-        if ($temp->data == $data) {
-            $this->head = $temp->next; // move pointer from head to the next node
-            return;
-        }
-        // traverse the linked list
-        while ($temp->next != null) {
-            if ($temp->next->data == $data) {
-                $temp->next = $temp->next->next;
-                return; // success and exit method
-            }
-            $temp = $temp->next; // move poiter to the next node
-        }
+    // Traverse the linked list
+    while ($temp->next != null) {
+      if ($temp->next->data == $data) {
+        $temp->next = $temp->next->next;
+        return; // success and exit method
+      }
+      $temp = $temp->next; // move poiter to the next node
     }
+  }
 
-
-    //display the content of the list
-    public function printList()
-    {
-        $temp = new Node();
-        $temp = $this->head;
-        if ($temp != null) {
-            echo "\nThe list contains: ";
-            while ($temp != null) {
-                echo $temp->data . " ";
-                $temp = $temp->next;
-            }
-        } else {
-            echo "\nThe list is empty.";
-        }
+  /**
+   * Traverse the linked list
+   *
+   * @return void
+   */
+  public function printList()
+  {
+    $temp = new Node();
+    $temp = $this->head;
+    if ($temp != null) {
+      echo "\nThe list contains: ";
+      while ($temp != null) {
+        echo $temp->data . " ";
+        $temp = $temp->next;
+      }
+    } else {
+      echo "\nThe list is empty.";
     }
+  }
 }
 $myList = new LinkedList();
 
--- search.php
+++ search.php
@@ -1,72 +1,34 @@
 <?php
-
-class Search2
+class Search
 {
-    private array $arr;
+  public $arr = [];
 
-    public function __construct()
-    {
-        $this->arr = [1, 2, 6, 8, 10];
+  public function __construct($arr = [])
+  {
+    $this->arr = $arr;
+  }
+
+  public function BinarySearch($value)
+  {
+    sort($this->arr);
+    $low = 0;
+    $high = count($this->arr) - 1; // end index off
+
+    while ($low <= $high) {
+      $middle = floor(($low + $high) / 2);
+      if ($value == $this->arr[$middle]) {
+        echo "At index $middle: ";
+        return true;
+      }
+
+      if ($value > $middle) { // ignore left side
+        $low = $middle + 1;
+      } else { // ignore right side
+        $high = $middle - 1;
+      }
     }
-
-    public function binarySearch($value): bool
-    {
-        sort($this->arr);
-        $left = 0; // first index
-        $right = count($this->arr) - 1; // last index
-
-        while ($left <= $right) { // value has searched must be between left and right and value can be the first or the last index then using operator '='
-            $middle = floor(($left + $right) / 2); // middle index of array
-
-            // if value == $middle value then return value has search and value
-            if ($value == $this->arr[$middle]) {
-                echo "At a index <b style='color: red'>$middle</b> of array, ";
-                return true;
-            }
-
-            // if value < middle value then ignore right side => range from before middle to left
-            if ($value < $this->arr[$middle]) {
-                $right = $middle - 1;
-            } else { // if value > middle then ignore left side => range from after middle to right
-                $left = $middle + 1;
-            }
-        }
-        // else value not have in array 👌👌👌👌
-        return false;
-    }
-
-    /**
-     * @return array
-     */
-    public function getArr(): array
-    {
-        return $this->arr;
-    }
-
-    /**
-     * @param array $arr
-     */
-    public function setArr(array $arr): void
-    {
-        $this->arr = $arr;
-    }
+    return false;
+  }
 }
-
-$binary = new Search2();
-
-// Set array
-$binary->setArr([9, 44, 622, 199, 33]);
-// Display an array
-echo '<pre>';
-echo "Display an array with sorting\n";
-$array = $binary->getArr();
-sort($array);
-print_r($array);
-
-$value = 622;
-$result = $binary->binarySearch($value);
-if ($result == false) {
-    echo "Value <b style='color: red'>$value</b> not exists in array";
-} else {
-    echo "value <b style='color: red'>$value</b> exists in array";
-}
+$binarySearch = new Search([1, 6, 8, 111, 59]);
+var_dump($binarySearch->binarySearch(111));
